f532467b08c66574f54602b66b7e7bfe915fc7e5
Nate Bush <nate_bush@Nates-MacBook-Air.local>
2020-06-20 13:57:45 -0700

    add requirements

--- src/main.py
+++ src/main.py
@@ -1,3 +1,22 @@
+from __future__ import print_function
+from __future__ import division
+import copy
+import os
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+from torchvision import datasets, models, transforms
+
+
+print("PyTorch Version: ", torch.__version__)
+print("Torchvision Version: ", torchvision.__version__)
+
+
 def main():
     pass
 
